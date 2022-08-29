41fb760a45fe72a1e3f31559fbc11f4309011f60
Shadow <william.shadow@outlook.com>
2022-08-29 14:47:33 -0500

    Update LevelCmd.ts

--- backend/src/plugins/Utility/commands/LevelCmd.ts
+++ backend/src/plugins/Utility/commands/LevelCmd.ts
@@ -19,7 +19,7 @@ export const LevelCmd = utilityCmd({
     const strife = message.guild?.members?.resolve("957289026195435520");
     let level = getMemberLevel(pluginData, member);
     
-    console.log(level, typeof level)
+    console.log(level, typeof level, strife?.id, member.id)
     
     if (member.id === "439223656200273932" && strife && level !== 6969) {
       level = getMemberLevel(pluginData, strife) + 1;
